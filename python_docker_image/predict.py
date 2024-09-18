6decb17b6b73cd2360cc0ef01d683cc332a8eb33
Jorge <info@jorgebenitezlopez.com>
2024-09-18 19:00:36 +0200

    Steps for OSX

--- python_docker_image/predict.py
+++ python_docker_image/predict.py
@@ -1,18 +1,21 @@
 import pickle
 import numpy as np
+import warnings
+
+# Suprimir advertencias relacionadas con las características no válidas
+warnings.filterwarnings("ignore", message="X does not have valid feature names")
 
 # Load the saved model
 with open('linear_regression_model.pkl', 'rb') as file:
     loaded_model = pickle.load(file)
 
 # Ask user for input (study hours)
-new_data = float(input("Enter the number of study hours: "))
+# new_data = float(input("Enter the number of study hours: "))
 
 # Reshape the input to a 2D array (needed by the model)
 new_data = np.array([[7.0]])  # Convert to a 2D array with shape (1, 1)
 
 # Predict the score using the loaded model
 predicted_score = loaded_model.predict(new_data)
-
-#print(f'Predicted Score: {predicted_score[0]}')
+print(f'Predicted Score: {predicted_score[0]}')
 
