4abf911cb3e9ddb0548fbf270d9f64188354d4cc
Alexandra Zambrano <alezambrano03@gmail.com>
2024-09-18 16:33:25 +0200

    fix: docker regression model pickle updated

--- python_docker_image/predict.py
+++ python_docker_image/predict.py
@@ -9,10 +9,10 @@ with open('linear_regression_model.pkl', 'rb') as file:
 new_data = float(input("Enter the number of study hours: "))
 
 # Reshape the input to a 2D array (needed by the model)
-new_data = np.array([[new_data]])  # Convert to a 2D array with shape (1, 1)
+new_data = np.array([[7.0]])  # Convert to a 2D array with shape (1, 1)
 
 # Predict the score using the loaded model
 predicted_score = loaded_model.predict(new_data)
 
-print(f'Predicted Score: {predicted_score[0]}')
+#print(f'Predicted Score: {predicted_score[0]}')
 
