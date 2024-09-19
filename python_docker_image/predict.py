cae6a9bfc5e3e250d8111e59a67f9cf1bedc4a36
Jorge <info@jorgebenitezlopez.com>
2024-09-19 09:19:57 +0200

    Interactive and with setp if stop

--- python_docker_image/predict.py
+++ python_docker_image/predict.py
@@ -10,8 +10,7 @@ with open('linear_regression_model.pkl', 'rb') as file:
     loaded_model = pickle.load(file)
 
 # Ask user for input (study hours)
-# new_data = float(input("Enter the number of study hours: "))
-
+new_data = float(input("Enter the number of study hours: "))
 # Reshape the input to a 2D array (needed by the model)
 new_data = np.array([[7.0]])  # Convert to a 2D array with shape (1, 1)
 
