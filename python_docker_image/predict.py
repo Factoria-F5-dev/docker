e0126189f461d248ec73eac8077f96be04f5af15
AlexandraZambrano <alezambrano03@gmail.com>
2024-10-14 08:12:58 +0200

    feat: predict.py and steps.txt updated

--- python_docker_image/predict.py
+++ python_docker_image/predict.py
@@ -10,9 +10,9 @@ with open('linear_regression_model.pkl', 'rb') as file:
     loaded_model = pickle.load(file)
 
 # Ask user for input (study hours)
-new_data = float(input("Enter the number of study hours: "))
+#new_data = float(input("Enter the number of study hours: "))
 # Reshape the input to a 2D array (needed by the model)
-# new_data = np.array([[7.0]])  # Convert to a 2D array with shape (1, 1)
+new_data = np.array([[7.0]])  # Convert to a 2D array with shape (1, 1)
 
 # Predict the score using the loaded model
 predicted_score = loaded_model.predict(new_data)
